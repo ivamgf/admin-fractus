28c48753b8f7d3d0bce3742dad6f21594592d058
ivamgf <programador.ivam@gmail.com>
2022-03-01 17:05:39 -0300

    add uploads pages

--- src/pages/dash/texts/index.tsx
+++ src/pages/dash/texts/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import CardUpload from '../../../components/Cards/CardUpload';
+import Footer from "../../../components/nav/Footer.tsx";
+import Header from "../../../components/nav/Header";
 
 const Texts = () => {
     return (
-        <div>
-            Textos
-        </div>
+        <>
+            <Main>
+                <Header />
+                <CardUpload />
+            </Main>
+            <Footer />
+        </>
     );
 }
+
+const Main = styled.div`
+    margin: 2em 1em;
+    min-height: 350px;
+`
 
 export default Texts;
